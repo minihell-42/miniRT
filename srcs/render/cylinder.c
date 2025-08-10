ef7aac1bf6709502fcecc8e221a74b868b65062f
Daniel Gomez Arango <dgomez-a@student.42berlin.de>
2025-08-10 14:50:17 +0200

    fix:cylinder centering

--- srcs/render/cylinder.c
+++ srcs/render/cylinder.c
@@ -46,8 +46,10 @@ static float	cylinder_side_intersection(t_ray *ray, t_cylinder *cy,
 	t_vector	oc_perp;
 	t_quadratic	quad;
 	float		dist_side;
-	float		height;
+	float		height_clamp;
+	float		half_height;
 
+	half_height = cy->height / 2.0f;
 	oc = vec_sub(ray->origin, cy->center);
 	dp = vec_sub(ray->direction, vec_scalar_mult(cy->normal,
 				vec_dot(ray->direction, cy->normal)));
@@ -62,8 +64,9 @@ static float	cylinder_side_intersection(t_ray *ray, t_cylinder *cy,
 	dist_side = pick_quad_root(&quad, RAY_DIST_MIN, dist_max);
 	if (dist_side <= 0.0f)
 		return (-1.0f);
-	height = vec_dot(vec_sub(ray_at(ray, dist_side), cy->center), cy->normal);
-	if (height < 0.0f || height > cy->height)
+	height_clamp = vec_dot(vec_sub(ray_at(ray, dist_side), cy->center),
+			cy->normal);
+	if (fabsf(height_clamp) > half_height + RAY_DIST_MIN)
 		return (-1.0f);
 	return (dist_side);
 }
@@ -76,15 +79,19 @@ int	cylinder_intersection(t_inter *hit, t_cylinder *cy)
 	float		dist_bott;
 	float		dist_top;
 	t_vector	top_center;
+	t_vector	bott_center;
 
 	prev = hit->dist;
 	dist_side = cylinder_side_intersection(&hit->ray, cy, hit->dist);
 	if (dist_side > RAY_DIST_MIN && dist_side < hit->dist)
 		hit->dist = dist_side;
-	dist_bott = cap_intersect(&hit->ray, cy, cy->center);
+	bott_center = vec_sub(cy->center, vec_scalar_mult(cy->normal, cy->height
+				/ 2.0f));
+	top_center = vec_add(cy->center, vec_scalar_mult(cy->normal, cy->height
+				/ 2.0f));
+	dist_bott = cap_intersect(&hit->ray, cy, bott_center);
 	if (dist_bott > RAY_DIST_MIN && dist_bott < hit->dist)
 		hit->dist = dist_bott;
-	top_center = vec_add(cy->center, vec_scalar_mult(cy->normal, cy->height));
 	dist_top = cap_intersect(&hit->ray, cy, top_center);
 	if (dist_top > RAY_DIST_MIN && dist_top < hit->dist)
 		hit->dist = dist_top;
@@ -97,12 +104,14 @@ t_vector	cylinder_normal(t_cylinder *cy, t_vector hit_point)
 	t_vector	oc;
 	t_vector	proj;
 	float		dist;
+	float		half_height;
 
+	half_height = cy->height / 2.0f;
 	oc = vec_sub(hit_point, cy->center);
 	dist = vec_dot(oc, cy->normal);
-	if (dist < RAY_DIST_MIN)
+	if (dist <= -half_height + RAY_DIST_MIN)
 		return (vec_negate(cy->normal));
-	if (dist > cy->height - RAY_DIST_MIN)
+	if (dist >= half_height - RAY_DIST_MIN)
 		return (cy->normal);
 	proj = vec_scalar_mult(cy->normal, dist);
 	return (vec_normalize(vec_sub(oc, proj)));
